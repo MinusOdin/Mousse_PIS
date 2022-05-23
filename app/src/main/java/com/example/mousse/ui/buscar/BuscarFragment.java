2d059d680442c6a1f0745d30f1c1eb1b71dae1d2
RakGH <rnunezpa11@alumnes.ub.edu>
2022-05-23 18:09:56 +0200

    buscar layout, BuscarViewModel i BuscarFragment

--- app/src/main/java/com/example/mousse/ui/buscar/BuscarFragment.java
+++ app/src/main/java/com/example/mousse/ui/buscar/BuscarFragment.java
@@ -1,37 +1,68 @@
 package com.example.mousse.ui.buscar;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.mousse.databinding.FragmentDashboardBinding;
+import com.example.mousse.CustomAdapter;
+import com.example.mousse.R;
+import com.example.mousse.Receta;
+import com.example.mousse.ui.perfil.PerfilViewModel;
+
+import java.util.ArrayList;
 
 public class BuscarFragment extends Fragment {
-
-    private FragmentDashboardBinding binding;
+    private RecyclerView mRecyclerView;
+    private BuscarViewModel viewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        BuscarViewModel buscarViewModel =
-                new ViewModelProvider(this).get(BuscarViewModel.class);
+        viewModel = new ViewModelProvider(this).get(BuscarViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View root = inflater.inflate(R.layout.fragment_buscar, container, false);
+        mRecyclerView = root.findViewById(R.id.recycleviewRecom);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()) );
+        ArrayList<Receta> recetas = new ArrayList<>();
+        CustomAdapter newAdapter = new CustomAdapter(root.getContext(), recetas);
+        mRecyclerView.setAdapter(newAdapter);
+        setLiveDataObservers(root);
 
-        final TextView textView = binding.textDashboard;
-        buscarViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    public void setLiveDataObservers(View root) {
+        //Subscribe the activity to the observable
+        viewModel.getRecetas().observe(getViewLifecycleOwner(), new Observer<ArrayList<Receta>>() {
+            @Override
+            public void onChanged(ArrayList<Receta> recetas) {
+                Log.d("Funciona lobserver de les receptes del perfil?", "Si");
+                CustomAdapter newAdapter = new CustomAdapter(root.getContext(), recetas);
+                mRecyclerView.setAdapter(newAdapter);
+            }
+        });
+
+        final Observer<String> observerToast = new Observer<String>() {
+            @Override
+            public void onChanged(String t) {
+                Toast.makeText(root.getContext(), t, Toast.LENGTH_SHORT).show();
+            }
+        };
+        viewModel.getToast().observe(getViewLifecycleOwner(), observerToast);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
     }
 }
--- app/src/main/java/com/example/mousse/ui/buscar/BuscarViewModel.java
+++ app/src/main/java/com/example/mousse/ui/buscar/BuscarViewModel.java
@@ -4,16 +4,52 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class BuscarViewModel extends ViewModel {
+import com.example.mousse.DatabaseAdapter;
+import com.example.mousse.Receta;
 
-    private final MutableLiveData<String> mText;
+import java.util.ArrayList;
+
+public class BuscarViewModel extends ViewModel implements DatabaseAdapter.vmInterface {
+
+    private final MutableLiveData<ArrayList<Receta>> mRecetas;
+    private final MutableLiveData<String> mToast;
+    private final DatabaseAdapter da;
 
     public BuscarViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mRecetas = new MutableLiveData<>();
+        mToast = new MutableLiveData<>();
+        da = new DatabaseAdapter(this);
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public void init(){
+        da.getCollectionSearch();
+    }
+
+    public LiveData<ArrayList<Receta>> getRecetas(){
+        return mRecetas;
+    }
+
+    public Receta getReceta(int idx){
+        return mRecetas.getValue().get(idx);
+    }
+
+    public LiveData<String> getToast(){
+        return mToast;
+    }
+
+
+    @Override
+    public void setCollection(ArrayList<Receta> recetas) {
+        mRecetas.setValue(recetas);
+    }
+
+    @Override
+    public void setToast(String t) {
+        mToast.setValue(t);
+    }
+
+    @Override
+    public void setSuccesfull(boolean succesfull) {
+
     }
 }
